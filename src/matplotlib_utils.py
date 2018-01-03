046f49fd032298976590057c6b814de0941b891d
Hiu Hong Yu <hiuyu@ucdavis.edu>
2018-01-03 01:04:19 -0800

    added track file

---
+++ src/matplotlib_utils.py
@@ -0,0 +1,23 @@
+################################################################################
+# This file contains function for easy use of Matplotlib feature			   #
+# Python Version: 3															   #
+# Matplotlib Version: 2.1.1													   #
+################################################################################
+
+# Standard Modules
+# import math
+
+# 3rd Party Modules
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+# Local Modules
+
+
+def show_image(image):
+	plt.imshow(image)
+	plt.show()
+
+def show_gray_image(image):
+	plt.imshow(image, cmap='gray')
+	plt.show()
